4e1b068bc92211eed6c68415eba13c528c2545a2
Quentin <quentin01.bource@gmail.com>
2023-01-05 15:57:01 +0100

    js

--- asset/js/dateUser.js
+++ asset/js/dateUser.js
@@ -11,3 +11,10 @@ let stock = await response.json();
 console.log(stock);
 
 }
+
+// import { eventsComplet } from "./asset/js/EventComplet.js";
+// import { dateUser } from "./asset/js/dateUser.js";
+
+// eventsComplet()
+// dateUser()
+// document.getElementById('test').addEventListener('click' , eventsComplet)
