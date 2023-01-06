ad73484ff2244383be6866cf9a36fef96cc48161
Quentin <quentin01.bource@gmail.com>
2023-01-06 11:37:24 +0100

    vis

---
+++ asset/js/vis.js
@@ -0,0 +1,15 @@
+
+
+
+    export function vis() {
+        let chooCity = document.getElementById('form')
+    
+        if  (chooCity.style.display == 'block')  {
+            chooCity.style.display = 'none'
+        }
+        else {
+            chooCity.style.display = 'block'
+        }
+        
+    }
+    
--- index.js
+++ index.js
@@ -1,54 +1,57 @@
 import { eventsComplet } from "./asset/js/EventComplet.js";
-//import { dateUser } from "./asset/js/dateUser.js";
+import { dateUser } from "./asset/js/dateUser.js";
+import { vis } from "./asset/js/vis.js";
 
 eventsComplet()
-//dateUser()
-document.getElementById('test').addEventListener('click' , eventsComplet)
+dateUser()
+// document.getElementById('test').addEventListener('click' , eventsComplet)
+
+document.getElementById('newEvent').addEventListener('click' , vis)
 
 
 
-const newEvent = document.getElementById("newEvent")
-const submitBtn = document.getElementById("createButton")
-const newDate = document.getElementById("newDateBtn")
-const datescontainer = document.getElementsByClassName("datescontainer")
+// const newEvent = document.getElementById("newEvent")
+// const submitBtn = document.getElementById("createButton")
+// const newDate = document.getElementById("newDateBtn")
+// const datescontainer = document.getElementsByClassName("datescontainer")
 
 
-newEvent.addEventListener("click", () => { document.getElementById("form").style.display = "block";
-})
+// newEvent.addEventListener("click", () => { document.getElementById("form").style.display = "block";
+// })
 
-/*newDate.addEventListener("click", () => {
-    let aDate = document.createElement("input");
-    aDate.type = "date";
-    aDate.className = "datelist";
-    datescontainer.appendChild(aDate);
-})*/
+// /*newDate.addEventListener("click", () => {
+//     let aDate = document.createElement("input");
+//     aDate.type = "date";
+//     aDate.className = "datelist";
+//     datescontainer.appendChild(aDate);
+// })*/
 
-document.getElementById("newDateBtn").addEventListener("click", addDate)
+// document.getElementById("newDateBtn").addEventListener("click", addDate)
 
-function addDate() {
-    let aDate = document.createElement("input");
-    aDate.type = "date";
-    aDate.className = "datelist";
-    //datescontainer.appendChild(aDate);
-    document.body.appendChild(aDate)
-}
+// function addDate() {
+//     let aDate = document.createElement("input");
+//     aDate.type = "date";
+//     aDate.className = "datelist";
+//     //datescontainer.appendChild(aDate);
+//     document.body.appendChild(aDate)
+// }
 
-submitBtn.addEventListener("click", () => {
+// submitBtn.addEventListener("click", () => {
 
-})
+// })
 
-function createEvent(name, author, description, dates) {
-    let event = document.createElement("div")
-    let name = document.getElementById("eventName").value
-    let nameField = document.createTextNode(name)
-    let author = document.getElementById("author").value
-    let authorField = document.createTextNode(author)
-    let description = document.getElementById("description").value
-    let descField = document.createTextNode(description)
-    let dates = []
-    event.appendChild(nameField)
-    event.appendChild(authorField)
-    event.appendChild(descField)
-}
+// function createEvent(name, author, description, dates) {
+//     let event = document.createElement("div")
+//     let name = document.getElementById("eventName").value
+//     let nameField = document.createTextNode(name)
+//     let author = document.getElementById("author").value
+//     let authorField = document.createTextNode(author)
+//     let description = document.getElementById("description").value
+//     let descField = document.createTextNode(description)
+//     let dates = []
+//     event.appendChild(nameField)
+//     event.appendChild(authorField)
+//     event.appendChild(descField)
+// }
 
  
