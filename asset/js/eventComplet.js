ad6777ba8082a69e6661030c749f73cf22fd549b
Quentin <quentin01.bource@gmail.com>
2023-01-09 14:25:27 +0100

    Del.js

--- asset/js/eventComplet.js
+++ asset/js/eventComplet.js
@@ -1,72 +1,74 @@
-export async function eventsComplet () {
-//récupération des données Json pour les events
-const response = await fetch(
-    `http://localhost:3000/api/events/`,
-{
-method: 'GET'
-}
- );
-let stock = await response.json();
+import { delEvent } from "./delEvent.js";
 
-console.log(stock);
+export async function eventsComplet() {
+    //récupération des données Json pour les events
+    const response = await fetch(
+        `http://localhost:3000/api/events/`,
+        {
+            method: 'GET'
+        }
+    );
+    let stock = await response.json();
 
-for (let elem of stock) {
+    console.log(stock);
 
-//créa la carte complète
-let newEvent = document.createElement('div')
-    newEvent.classList.add('event')
-    document.getElementById('main').appendChild(newEvent)
+    for (let elem of stock) {
 
-//créa titre 
-let newEventTitle = document.createElement('h2')
-    newEventTitle.classList.add('eventTitle')
-    newEventTitle.appendChild(document.createTextNode(elem.name))
-    newEvent.appendChild(newEventTitle)
+        //créa la carte complète
+        let newEvent = document.createElement('div')
+        newEvent.classList.add('event')
+        document.getElementById('main').appendChild(newEvent)
 
-//créa auteur 
-let newEventAuteur = document.createElement('span')
-    newEventAuteur.classList.add('eventAuteur')
-    newEventAuteur.appendChild(document.createTextNode( " (" + elem.author + ") "))
-    newEventTitle.appendChild(newEventAuteur)
+        //créa titre 
+        let newEventTitle = document.createElement('h2')
+        newEventTitle.classList.add('eventTitle')
+        newEventTitle.appendChild(document.createTextNode(elem.name))
+        newEvent.appendChild(newEventTitle)
 
-//créa description 
-let newEventDesc = document.createElement('p')
-    newEventDesc.classList.add('eventDesc')
-    newEventDesc.appendChild(document.createTextNode(elem.description))
-    newEvent.appendChild(newEventDesc)
+        //créa auteur 
+        let newEventAuteur = document.createElement('span')
+        newEventAuteur.classList.add('eventAuteur')
+        newEventAuteur.appendChild(document.createTextNode(" (" + elem.author + ") "))
+        newEventTitle.appendChild(newEventAuteur)
 
-//créa date 
-let newEventAllDate = document.createElement('div')
-    newEventAllDate.classList.add('eventAllDate')
-    newEvent.appendChild(newEventAllDate)
+        //créa description 
+        let newEventDesc = document.createElement('p')
+        newEventDesc.classList.add('eventDesc')
+        newEventDesc.appendChild(document.createTextNode(elem.description))
+        newEvent.appendChild(newEventDesc)
+
+        //créa date 
+        let newEventAllDate = document.createElement('div')
+        newEventAllDate.classList.add('eventAllDate')
+        newEvent.appendChild(newEventAllDate)
 
 
-         
+
         for (let dates of elem.dates) {
-            
+
             let newEventDate = document.createElement('div')
-                newEventDate.classList.add('eventDate')
-                newEventAllDate.appendChild(newEventDate)
-                newEventDate.appendChild(document.createTextNode(dates.date))
+            newEventDate.classList.add('eventDate')
+            newEventAllDate.appendChild(newEventDate)
+            newEventDate.appendChild(document.createTextNode(dates.date))
 
-                // créa user 
-                for (let attendees of dates.attendees) {
+            // créa user 
+            for (let attendees of dates.attendees) {
 
-                    let newEventAttendees = document.createElement('div')
-                    newEventAttendees.classList.add('eventAttendees')
-                    newEventAllDate.appendChild(newEventAttendees)
-                    newEventAttendees.appendChild(document.createTextNode(attendees.name))
+                let newEventAttendees = document.createElement('div')
+                newEventAttendees.classList.add('eventAttendees')
+                newEventAllDate.appendChild(newEventAttendees)
+                newEventAttendees.appendChild(document.createTextNode(attendees.name))
 
-                    
-                }
+
+            }
 
         }
+      
 
 
 
 
-
-}
+    }
 
 
 }
