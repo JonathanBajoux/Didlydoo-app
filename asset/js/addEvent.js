02a6e8b7765e00866ecc3cd8cee04d180dd66477
Quentin <quentin01.bource@gmail.com>
2023-01-09 12:08:15 +0100

    post

--- asset/js/addEvent.js
+++ asset/js/addEvent.js
@@ -1,23 +1,27 @@
 export function addEvent() {
-    
-    let completEvent = { 
-       "name" : "Test", 
-       "dates" : ['2024-06-03'], 
-       "author" : "Didier", 
-       "description" : "Ceci est un test pour voir si tout est ok" }
-   
-        console.log(completEvent)
 
-     fetch("http://localhost:3000/api/events/", {
-       method: "POST",
-       body: JSON.stringify({completEvent
-       }),
+    let completEvent = {
+        name: "Test",
+        dates: ['2024-06-03', '2025-07-03'],
+        author: "Didier",
+        description: "Ceci est un test pour voir si tout est ok"
+    };
+
+    console.log(JSON.stringify(completEvent));
+
+
+    fetch('http://localhost:3000/api/events/', {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(completEvent
+        ),
+
        
-       headers: {
-         "Content-type": "application/json;"
-       }
-     })
-       .then((response) => response.json())
-       .then((json) => console.log(json));
+    })
+        .then((response) => response.json())
+        .then((json) => console.log(json))
+        .catch((error) => { console.error('Error', error) })
 }
 
